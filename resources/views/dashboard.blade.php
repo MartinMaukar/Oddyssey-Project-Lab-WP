99cc309a54d247a22f20c6bd4c7fffb18dd6fd41
Jazimanuel <jason.imanuel@binus.ac.id>
2022-06-27 22:11:46 +0700

    update bootstrap

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,8 @@
 @extends('template.navbar')
     @section('title','dashboard')
 @section('content')
+<form class="d-flex" role="search">
+    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success" type="submit">Search</button>
+</form>
+@endsection
