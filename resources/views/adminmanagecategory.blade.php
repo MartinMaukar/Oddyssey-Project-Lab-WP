927d08b66e2278e8e2fcc40a2b0b135fac1c8bee
martinmaukar <martinmaukar@gmail.com>
2022-07-21 15:22:00 +0800

    css

--- resources/views/adminmanagecategory.blade.php
+++ resources/views/adminmanagecategory.blade.php
@@ -24,24 +24,40 @@
     <button type="button" class="btn btn-dark d-flex justify-content-center align-items-center m-3">Add Category</button>
 </a>
 
-@foreach($categories as $item)
-<div class="card mb-3" style="width: 80%;display:flex; flex-direction:column">
 
-      <div class="col-md-8">
-        
-        <div class="card-body">
-            <h5 class="card-text">{{ $item->category_name }}</h5>
 
-            
-            <a class="btn btn-secondary" href="/adminupdatecategory/{{ $item->id }}" role="button">Update</a>
-            <a class="btn btn-danger" href="/removecategory/{{ $item->id }}" role="button">Remove</a>    
-
-        </div>
-        
-      </div>
-
+<div class="d-inline-flex p-2 flex-column ">
+  @foreach($categories as $item)
+  <div class="card border-dark mb-3 mx-1" style="max-width: 18rem;">
+    <div class="" style="text-decoration: none">
+      <div class="card-header"><strong>{{ $item->category_name }}</strong></div>
+    </div>
+    
+    <div class="card-body text-dark">
+      <a class="btn btn-secondary" href="/adminupdatecategory/{{ $item->id }}" role="button">Update</a>
+      <a class="btn btn-danger" href="/removecategory/{{ $item->id }}" role="button">Remove</a>  
+    </div>
   </div>
 @endforeach
+</div>
+
 
 
 @endsection
+
+{{-- <div class="card mb-3" style="width: 80%;display:flex; flex-direction:column">
+
+  <div class="col-md-8">
+    
+    <div class="card-body">
+        <h5 class="card-text">{{ $item->category_name }}</h5>
+
+        
+        <a class="btn btn-secondary" href="/adminupdatecategory/{{ $item->id }}" role="button">Update</a>
+        <a class="btn btn-danger" href="/removecategory/{{ $item->id }}" role="button">Remove</a>    
+
+    </div>
+    
+  </div>
+
+</div> --}}
